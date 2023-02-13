c3dd33f289d0b9d7134a9a5e9532e6261ed5997f
DANIELLIMBONG31\ASUS <aska.limbong@gmail.com>
2023-02-13 16:58:34 +0700

    submitin test 2

--- Test 2/test2.php
+++ Test 2/test2.php
@@ -19,17 +19,26 @@ for($i = 1; $i<=$gits; $i++){
 
 for($a = 0; $a<count($nilaiGits)-1; $a++){
     for($j =0; $j<count($nilaiPesertas)-1; $j++){
+        
         if($nilaiGits[$a] > $nilaiPesertas[$j]){
-            if($j-1 == 0){
-                echo $j+1;
+            
+            if($nilaiGits[$a] = $nilaiPesertas[$j]){
+                echo " ".$j;
             }
+            if($nilaiGits[$a] > $nilaiPesertas[$j]){
+                if($j-1 == 0){
+                    echo " ".$j+1;
+                }
+                else{
+                    echo " ".$j-1;
+                }
+            }
+            if($nilaiGits[$a] < $nilaiPesertas[$j]){
+                echo " ".$j+1;
+            }
+            
         }
-        if($nilaiGits[$a] < $nilaiPesertas[$j]){
-             echo " ".$j+1;
-        }
-        if($nilaiGits[$a] < $nilaiPesertas[$j]){
-             echo " ".$j;
-        }
+        
     }
 }
 
